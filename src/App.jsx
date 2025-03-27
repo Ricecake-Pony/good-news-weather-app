9d550137ec804d9ed1cb77d752710ec823554c9d
Trey D. <trey.delcamp@gmail.com>
2025-03-27 14:42:50 -0600

    Successfully tested/connected useGeoLocation to our WeatherContext and fetched data from WeatherAPI on mount.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 export default function App() {
   return (
     <>
@@ -7,7 +6,6 @@ export default function App() {
       Weather app starting point!
     </div>
     <div className="bg-indigo-700 text-white p-4 rounded-xl">Tailwind is working!</div>
-
     </>
 
   );
--- src/context/WeatherContext.jsx
+++ src/context/WeatherContext.jsx
@@ -1,14 +1,39 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import useGeoLocation from "../hooks/useGeoLocation";
+import axios from "axios";
+
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 export const WeatherContext = createContext();
 
 export function WeatherProvider({ children }) {
 	const location = useGeoLocation();
-	const { latitude: lat, longitude: lon } = location || {};
-	console.log("Lat:", lat, "Lon:", lon);
-	const [weatherData, setWeatherData] = useState(null);
 
+	useEffect( () => {
+		const { latitude: lat, longitude: long } = location || {};
+		const params = new URLSearchParams({
+			key: `${apiKey}`,
+			q: `${lat} ${long}`,
+			days: 3,
+		});
+
+		const url = `https://api.weatherapi.com/v1/forecast.json?${params.toString()}`;
+		async function fetchLocationData (){
+			if (!lat || !long) return;
+			try{
+				const res = await axios.get(url)
+				const data = res.data
+				setWeatherData(data)
+				console.log(data)
+			}
+			catch(err){
+				console.error("the error is:", err)
+			}
+		}
+		fetchLocationData()
+	}, [location])
+
+	const [weatherData, setWeatherData] = useState(null);
 
 	return (
 		<WeatherContext.Provider value={{ weatherData, setWeatherData }}>
