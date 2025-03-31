482e3f78e94c3f4edf687ec54c7044c19d6e3b02
Trey D. <trey.delcamp@gmail.com>
2025-03-31 14:13:12 -0600

    Finished CurrentLocationTile and prepared for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
 import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom'
+import "./index.css";
 import App from "./App.jsx";
 import { WeatherProvider } from './context/WeatherContext.jsx';
 
 createRoot(document.getElementById("root")).render(
+	<BrowserRouter>
 	<WeatherProvider>
 		<App />
 	</WeatherProvider>
+	</BrowserRouter>
 );
