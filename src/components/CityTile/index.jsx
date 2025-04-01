6b2925f7fc5afa43d9496879f92a314313a937a6
Trey D. <trey.delcamp@gmail.com>
2025-04-01 14:45:41 -0600

    Create and render static CityTile component in Sidebar

---
+++ src/components/CityTile/index.jsx
@@ -0,0 +1,10 @@
+export default function CityTile ({city, temp}){
+
+    return(
+        <>
+        {city}
+        {temp}
+        
+        </>
+    )
+}
--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import CurrentLocationTile from "../CurrentLocationTile";
+import CityTile from "../CityTile";
 import "./sidebar.css";
 
 export default function Sidebar() {
@@ -7,6 +8,7 @@ export default function Sidebar() {
 		<div className="sidebar">
 			<CurrentLocationTile />
 			<h2>WeatherNav</h2>
+            <CityTile temp="55 F" city="Tokyo"/>
 			<nav>
 				<ul>
 					<li>
