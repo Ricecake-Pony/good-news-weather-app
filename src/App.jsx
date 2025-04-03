f20b11a4e9d2947351484cd0f414fa0ecf4a99df
Trey D. <trey.delcamp@gmail.com>
2025-04-02 20:30:01 -0600

    create Layout Component with nested routing and ensure persistent Sidebar via Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import CityPage from "./pages/CityPage";
 import HomePage from "./pages/HomePage";
-import Sidebar from "./components/SideBar";
 import { WeatherContext } from "./context/WeatherContext";
 import fetchRegionalBackground from "./utils/fetchRegionalBackground";
 
@@ -28,10 +27,20 @@ export default function App() {
 
 	return (
 		<>
-			<Sidebar />
 			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/city/:cityName" element={<CityPage />} />
+				<Route
+					path="/"
+					element={<Layout />}
+				>
+					<Route
+						index
+						element={<HomePage />}
+					/>
+					<Route
+						path="city/:cityName"
+						element={<CityPage />}
+					/>
+				</Route>
 			</Routes>
 		</>
 	);
