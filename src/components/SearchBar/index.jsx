faa25bbf82be8de04edeaab89401e32fbcc8ed1f
Trey D. <trey.delcamp@gmail.com>
2025-04-13 17:47:06 -0600

    implement Tailwind in SearchBar in order to follow the  glassmorphism design

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 
 export default function SearchBar({ onSearch }) {
-	const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (!input) return;
-		if (input.length < 2) return;
-		onSearch(input.toLowerCase().trim());
-		setInput("");
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input) return;
+    if (input.length < 2) return;
+    onSearch(input.toLowerCase().trim());
+    setInput("");
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				value={input}
-				onChange={(e) => setInput(e.target.value)}
-			/>
-			<button type="submit">Search</button>
-		</form>
-	);
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white/10 backdrop-blur-md rounded-xl p-3 flex items-center gap-2 shadow-md"
+    >
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="bg-transparent text-white placeholder-white/60 focus:outline-none w-full"
+      />
+      <button
+        type="submit"
+        className=" cursor-pointer px-3 py-1 bg-white/20 rounded-md text-white hover:bg-white/30 transition"
+      >
+        Search
+      </button>
+    </form>
+  );
 }
